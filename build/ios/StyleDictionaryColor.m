0a94fb5957828598d467ffa502983595af7f334c
Automated Publisher <actions@users.noreply.github.com>
2021-02-03 21:43:29 +0000

    Automated publish: Wed Feb  3 21:43:29 UTC 2021 ccc8b982ae0dae45e87169601caee9a08e7e598e

--- build/ios/StyleDictionaryColor.m
+++ build/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Wed, 03 Feb 2021 21:28:43 GMT
+// Generated on Wed, 03 Feb 2021 21:43:28 GMT
 //
 
 #import "StyleDictionaryColor.h"
@@ -21,7 +21,7 @@
 
   dispatch_once(&onceToken, ^{
     colorArray = @[
-rgba(120, 84, 81, 1),
+rgba(119, 136, 85, 1),
 rgba(0, 100, 250, 1),
 rgba(14, 97, 222, 1),
 rgba(0, 100, 250, 0.1),
--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -3,7 +3,7 @@
 // StyleDictionarySize.m
 //
 // Do not edit directly
-// Generated on Wed, 03 Feb 2021 21:28:43 GMT
+// Generated on Wed, 03 Feb 2021 21:43:28 GMT
 //
 
 #import "StyleDictionarySize.h"
